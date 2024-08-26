4301d716dae4935adabb7a79625b57d540e47e75
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2024-08-26 16:07:28 +0100

    chore: Refactor main function to remove unused arguments

--- src/core/main.c
+++ src/core/main.c
@@ -6,20 +6,17 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 21:54:22 by muabdi            #+#    #+#             */
-/*   Updated: 2024/08/25 17:09:27 by muabdi           ###   ########.fr       */
+/*   Updated: 2024/08/26 15:57:37 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-int	main(int argc, char *argv[], char *envp[])
+int	main(void)
 {
 	t_prompt	*prompt;
 	char		*buffer;
 
-	(void)argc;
-	(void)argv;
-	(void)envp;
 	prompt = initialize_prompt();
 	if (!prompt)
 		return (EXIT_FAILURE);
