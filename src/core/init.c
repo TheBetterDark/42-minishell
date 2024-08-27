79e525d3fb220413799520722c20a4f3d5f18d4d
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2024-08-27 23:17:22 +0100

    chore: Remove unused colour.h file and update main.c and signals.c filenames

--- src/core/init.c
+++ src/core/init.c
@@ -1,18 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   signals.c                                          :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/26 22:51:08 by muabdi            #+#    #+#             */
-/*   Updated: 2024/08/26 23:02:29 by muabdi           ###   ########.fr       */
+/*   Created: 2024/08/27 19:49:04 by muabdi            #+#    #+#             */
+/*   Updated: 2024/08/27 20:52:56 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
 static void	handle_sigint(int sig);
+
+t_data	*initialize_shell(void)
+{
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
+	if (!data)
+		return (perror("Error allocating memory"), NULL);
+	data->user = getenv("USER");
+	if (data->user == NULL)
+		return (perror("Error getting USER environment variable"), free(data),
+			NULL);
+	if (ft_gethostname(data->hostname) == -1)
+		return (perror("Error getting hostname"), free(data), NULL);
+	if (ft_getcwd(data->cwd, true) == -1)
+		return (perror("Error getting current working directory"), free(data),
+			NULL);
+	return (data);
+}
 
 int	initalize_signals(void)
 
--- src/core/main.c
+++ src/core/main.c
@@ -6,34 +6,43 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 21:54:22 by muabdi            #+#    #+#             */
-/*   Updated: 2024/08/26 23:04:58 by muabdi           ###   ########.fr       */
+/*   Updated: 2024/08/27 20:55:55 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+static char	*display_prompt(t_data *data);
+
 int	main(void)
 {
-	t_prompt	*prompt;
-	char		*buffer;
+	t_data	*data;
 
-	prompt = initialize_prompt();
-	if (!prompt)
+	data = initialize_shell();
+	if (!data)
 		return (EXIT_FAILURE);
 	if (initalize_signals() == -1)
-		return (EXIT_FAILURE);
+		return (free(data), EXIT_FAILURE);
 	while (true)
 	{
-		buffer = display_prompt(prompt);
-		if (!buffer || ft_strncmp(buffer, "exit", 4) == 0)
-		{
-			printf("exit\n");
-			if (buffer)
-				free(buffer);
+		data->input = display_prompt(data);
+		if (!data->input)
 			break ;
-		}
-		free(buffer);
+		free(data->input);
 	}
-	free(prompt);
+	free(data);
+	printf("exit\n");
 	return (EXIT_SUCCESS);
+}
+
+static char	*display_prompt(t_data *data)
+{
+	char	prompt_str[256];
+	char	*input;
+
+	ft_snprintf(prompt_str, sizeof(prompt_str),
+		USR "%s@%s" RST ":" CWD "%s" RST "$ ", data->user,
+		data->hostname, data->cwd);
+	input = readline((const char *)prompt_str);
+	return (input);
 }
