60845a4d3fb4aa36082dfea6cd7fce4510260802
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-01-19 20:46:49 +0000

    1) A command line does not need a command, it can be only redirections. Each redirection will be opened. 2) If a delimiter has unfinished quotes it cannot be exited. 3) A delimiter can have any number of closed quotes and content of the quotes are used. For example ``` cat <<'A''B''C' ``` the delimiter is ABC. 3) If ``` cat <<'A'' ``` the delimiter is A' .

--- src/executor/exec_get_heredocs.c
+++ src/executor/exec_get_heredocs.c
@@ -6,7 +6,7 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:35:25 by smoore            #+#    #+#             */
-/*   Updated: 2025/01/18 20:50:05 by smoore           ###   ########.fr       */
+/*   Updated: 2025/01/19 19:52:15 by smoore           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,87 @@ static void	heredoc_sigint(int sig)
 	exit(EXIT_FAILURE);
 }
 
+bool	check_syntax(int single_quotes, int double_quotes, int len)
+{
+	if (!single_quote && !double_quote)
+		return (true);
+	if (single_quote == 1 || double_quote == 1)
+		return (false);
+	if ((single_quote == 2 && double_quote == 0)
+		|| (single_quote == 0 && double_quote == 2))
+	{
+		if ((eof[len] == '\'' && eof[len - 1] == '\'')
+			|| (eof[len] == '\"' && eof[len - 1] == '\"'))
+			return (true);
+	}
+	return (false);
+}
+
+bool	is_valid_delim(char *eof)
+{
+	int		single_quotes;
+	int		double_quotes;
+	int		len;
+	bool	result;
+
+	result = true;
+	single_quotes = 0;
+	double_quotes = 0;
+	len = ft_strlen(eof);
+	while (*eof)
+	{
+		if (*eof == '\'')
+			single_quote++;
+		if (*eof == '\"')
+			double_quote++;
+		eof++;
+	}
+	result = check_syntax(single_quotes, double_quotes, len);
+	return (result);
+}
+
+void	begin_heredoc_with_delimiter(t_data *data, t_cmd *cur_cmd)
+{
+	ft_putstr_fd("> ", STDOUT_FILENO);
+	buf = get_next_line(STDIN_FILENO);
+	buf_toks = ft_split(buf, ' ');
+	while (*buf_toks)
+	{
+		save = buf;
+		*buf_toks = try_env_value(*buf_toks, data->env, data->exit_stat);
+		buf = ft_strjoin(save, *buf_toks);
+		buf_toks++;
+	}
+	close(cur->pipe_fds[0]);
+	ft_putstr_fd(buf, cur->pipe_fds[1]);
+	free(buf);
+}
+
+void	check_delimiter(t_data *data, t_cmd *cur_cmd)
+{
+	char *buf;
+
+	if (is_valid_delim(cur_cmd->ins->eof))
+		begin_heredoc_with_delim(data, cur_cmd);
+	else
+	{
+		while (true)
+		{
+			ft_putstr("> ", STDOUT_FILENO);
+			buf = get_next_line(STDIN_FILENO);
+			free(buf);
+		}
+	}
+}
+
 void	get_heredocs(t_data *data, t_cmd *cur_cmd)
 {
 	t_ins	*cur;	
 	pid_t	pid;
 	int		wstatus;
+	char	**buf_toks;
 	char	*buf;
+	char	*save;
 
 	if (!cur_cmd->ins)
 		return ;
@@ -39,12 +114,7 @@ void	get_heredocs(t_data *data, t_cmd *cur_cmd)
 			if (pid == 0)
 			{
 				signal(SIGINT, heredoc_sigint);
-				ft_putstr_fd("> ", STDOUT_FILENO);
-				buf = get_next_line(STDIN_FILENO);
-				// expand VARS
-				close(cur->pipe_fds[0]);
-				ft_putstr_fd(buf, cur->pipe_fds[1]);
-				free(buf);
+				check_delimiter(data, cur);
 			}
 		}
 		else
