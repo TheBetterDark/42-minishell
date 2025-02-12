7bdcd415ac622b0c967f4c32b4260bf46ef2bc2c
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2025-02-12 19:05:37 +0000

    Implement error handling for exit command arguments and update exit status

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:58:29 by smoore            #+#    #+#             */
-/*   Updated: 2025/02/07 18:11:26 by smoore           ###   ########.fr       */
+/*   Updated: 2025/02/12 18:43:50 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,22 @@
 
 void	builtin_exit(t_data *data);
 
+// TODO: Error check if arguments are not numbers
 void	builtin_exit(t_data *data)
 {
-	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	int	exit_stat;
+
+	if (data->cmds && data->cmds->cmdv && data->cmds->cmdv[1])
+	{
+		if (data->cmds->cmdv[2])
+		{
+			ft_printf("minishell: exit: too many arguments\n");
+			return ;
+		}
+		exit_stat = ft_atoi(data->cmds->cmdv[1]);
+	}
+	else
+		exit_stat = data->exit_stat;
 	ft_str_arr_free(&data->env);
 	free_minishell(data);
 	if (data->pwd)
@@ -24,5 +37,6 @@ void	builtin_exit(t_data *data)
 	close(data->saved_stdin);
 	close(data->saved_stdout);
 	free(data);
-	exit(EXIT_SUCCESS);
+	ft_printf("exit\n");
+	exit(exit_stat);
 }
