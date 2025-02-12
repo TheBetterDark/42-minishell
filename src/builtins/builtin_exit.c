6119834968efeef761f075093ca7aba456855d46
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2025-02-12 19:30:48 +0000

    Add error handling for exit command arguments and validate numeric input

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -6,26 +6,53 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 11:58:29 by smoore            #+#    #+#             */
-/*   Updated: 2025/02/12 18:43:50 by muabdi           ###   ########.fr       */
+/*   Updated: 2025/02/12 19:30:43 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/data.h"
 
-void	builtin_exit(t_data *data);
+void		builtin_exit(t_data *data);
 
-// TODO: Error check if arguments are not numbers
+static bool	ft_isnumber(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static void	check_exit_args(t_cmd *cmd)
+{
+	if (cmd->cmdv[2])
+	{
+		ft_printf("minishell: exit: too many arguments\n");
+		return ;
+	}
+	if (!ft_isnumber(cmd->cmdv[1]))
+	{
+		ft_printf("minishell: exit: %s: numeric argument required\n",
+			cmd->cmdv[1]);
+		exit(2);
+	}
+}
+
+// TODO: if the argument number is greater than intmax
 void	builtin_exit(t_data *data)
 {
 	int	exit_stat;
 
 	if (data->cmds && data->cmds->cmdv && data->cmds->cmdv[1])
 	{
-		if (data->cmds->cmdv[2])
-		{
-			ft_printf("minishell: exit: too many arguments\n");
-			return ;
-		}
+		check_exit_args(data->cmds);
 		exit_stat = ft_atoi(data->cmds->cmdv[1]);
 	}
 	else
