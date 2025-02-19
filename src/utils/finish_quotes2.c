7961439b5730c048bcc5a4b748d8fc70184ecbeb
muabdi <91607604+TheBetterDark@users.noreply.github.com>
2025-02-19 14:58:48 +0000

    Refactor quoted input completion logic and separate quote checking into a new function

--- src/utils/finish_quotes2.c
+++ src/utils/finish_quotes2.c
@@ -6,37 +6,46 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 12:14:43 by smoore            #+#    #+#             */
-/*   Updated: 2025/02/19 14:05:24 by muabdi           ###   ########.fr       */
+/*   Updated: 2025/02/19 14:57:09 by muabdi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/data.h"
 
-char	*complete_quoted_input(char *input);
+char		*complete_quoted_input(char *input);
 
-char	*complete_quoted_input(char *input)
+static char	*check_and_complete_quotes(char *input)
 {
+	char	*new_input;
 	int		i;
 	int		delim;
 
-	if (!has_quotes(input))
-		return (input);
 	i = 0;
 	while (input[i])
 	{
-		if (input[i] == '\'' || input[i] == '\"')
+		if (input[i] != '\'' && input[i] != '\"')
 		{
-			delim = input[i];
 			i++;
-			while (input[i] && input[i] != delim)
-				i++;
-			if (!input[i])
-				return (free(input), finish_quotes(input));
-			else
-				i++;
+			continue ;
+		}
+		delim = input[i];
+		i++;
+		while (input[i] && input[i] != delim)
+			i++;
+		if (!input[i])
+		{
+			new_input = finish_quotes(input);
+			return (free(input), new_input);
 		}
 		else
 			i++;
 	}
 	return (input);
+}
+
+char	*complete_quoted_input(char *input)
+{
+	if (!has_quotes(input))
+		return (input);
+	return (check_and_complete_quotes(input));
 }
