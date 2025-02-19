088d29b888f9a9751b61192e741b489e61b0a4ae
mooreApps22 <124527392+mooreApps22@users.noreply.github.com>
2025-02-19 19:45:08 +0000

    Fixes after final testing.

--- src/builtins/builtin_export.c
+++ src/builtins/builtin_export.c
@@ -6,34 +6,32 @@
 /*   By: muabdi <muabdi@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 16:35:25 by smoore            #+#    #+#             */
-/*   Updated: 2025/02/14 11:31:36 by muabdi           ###   ########.fr       */
+/*   Updated: 2025/02/19 19:43:11 by smoore           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/data.h"
 
-static void	update_existing_var(char *cur_env_var, char *export_str);
+static void	update_existing_var(t_data *data, char *cur_env_var, char *export_str);
 static void	add_new_var(t_data *data, char *export_str);
 void		builtin_export(t_data *data, char **export_str);
 
-static void	update_existing_var(char *cur_env_var, char *export_str)
+static void	update_existing_var(t_data *data, char *cur_env_var, char *export_str)
 {
 	size_t	new_value_len;
-	char	*new_var;
+	char	**new_env;
 
 	new_value_len = ft_strlen(export_str);
 	if (new_value_len <= ft_strlen(cur_env_var))
 		ft_strcpy(cur_env_var, export_str);
 	else
 	{
-		new_var = ft_strdup(export_str);
-		if (!new_var)
-		{
-			perror("export failed");
-			exit(EXIT_FAILURE);
-		}
-		free(cur_env_var);
-		cur_env_var = new_var;
+		new_env = ft_str_arr_minus((const char **)data->env, cur_env_var);
+		ft_str_arr_free(&data->env);
+		data->env = new_env;
+		new_env = ft_str_arr_add((const char **)data->env, export_str);
+		ft_str_arr_free(&data->env);
+		data->env = new_env;
 	}
 }
 
@@ -70,7 +68,7 @@ void	builtin_export(t_data *data, char **export_str)
 		}
 		cur_env_var = get_environment_variable(*export_str, data);
 		if (cur_env_var)
-			update_existing_var(cur_env_var, *export_str);
+			update_existing_var(data, cur_env_var, *export_str);
 		else
 			add_new_var(data, *export_str);
 		export_str++;
